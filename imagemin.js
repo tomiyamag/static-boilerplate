79f36c8fa36e27faeebb41598b202bb61283acc2
tomiyamag <tomiyama@nadia.bz>
2022-05-17 20:01:49 +0900

    fix: imagemin

--- imagemin.js
+++ imagemin.js
@@ -3,8 +3,11 @@ const mozjpeg = require("imagemin-mozjpeg");
 const pngquant = require("imagemin-pngquant");
 const gifsicle = require("imagemin-gifsicle");
 const svgo = require("imagemin-svgo");
+const changed = process.argv[2];
+const file =
+  changed !== "" && changed ? [changed] : ["./src/assets/images/**/*.*"];
 
-keepFolder(["./src/assets/images/**/*.*"], {
+keepFolder(file, {
   plugins: [
     mozjpeg({
       quality: 80,
